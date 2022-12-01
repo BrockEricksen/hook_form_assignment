7a3c3e1408b9893da9ec6b7f3c4fa88989ada70f
Brock Ericksen <brockericksen@gmail.com>
2022-12-01 18:55:38 -0500

    Coding dojo react assignment.

---
+++ src/components/Form.js
@@ -0,0 +1,42 @@
+import React, { useState } from 'react';
+
+const Form = (props) => {
+    const [firstName, setFirstName] = useState("");
+    const [lastName, setLastName] = useState("");
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+    const [confPassword, setConfPassword] = useState("");
+    
+    return(
+        <form>
+            <div>
+                <label>First Name: </label>
+                <input type="text" onChange={ (e) => setFirstName(e.target.value) } />
+            </div>
+            <div>
+                <label>Last Name: </label>
+                <input type="text" onChange={ (e) => setLastName(e.target.value) } />
+            </div>
+            <div>
+                <label>Email Address: </label> 
+                <input type="text" onChange={ (e) => setEmail(e.target.value) } />
+            </div>
+            <div>
+                <label>Password: </label>
+                <input type="password" onChange={ (e) => setPassword(e.target.value) } />
+            </div>
+            <div>
+                <label>Confirm Password: </label>
+                <input type="password" onChange={ (e) => setConfPassword(e.target.value) } />
+            </div>
+
+            <h4>First Name: {firstName}</h4>
+            <h4>Last Name: {lastName}</h4>
+            <h4>Email: {email}</h4>
+            <h4>Password: {password}</h4>
+            <h4>Confirm Password: {confPassword}</h4>
+        </form>
+    )
+}
+
+export default Form;
